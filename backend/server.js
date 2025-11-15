b0f4af4be8dd0326bbb18db73338f3942c097a51
Lugune-dev <luguneshadrack@gmail.com>
2025-11-15 19:52:54 +0300

    some changes is done

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ filename: (req, file, cb) => {
 });
 const upload = multer({ storage });
 
-const angularDistPath = path.join(__dirname, 'backend/dist/portal'); // <-- updated path
+const angularDistPath = path.join(__dirname, 'dist/portal'); // <-- updated path
 app.use(express.static(angularDistPath));
 
 const db = mysql.createConnection({
