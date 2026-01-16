137ec7558ffd068648b6a47ca5776c803c759753
Lugune-dev <luguneshadrack@gmail.com>
2026-01-16 12:25:11 +0300

    some change is done

--- tphpa/src/app/announcement/announcement.ts
+++ tphpa/src/app/announcement/announcement.ts
@@ -1,22 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface NewsItem {
-  id: number;
-  title: string;
-  category: string;
-  date: string;
-  content: string;
-}
-
-@Injectable({ providedIn: 'root' })
-export class NewsService {
-  private apiUrl = 'https://portal-api-z927.onrender.com/api/announcement';
-
-  constructor(private http: HttpClient) {}
-
-  getPublicNews(): Observable<NewsItem[]> {
-    return this.http.get<NewsItem[]>(`${this.apiUrl}/public`);
-  }
+@Component({
+  selector: 'app-announcement',
+  imports: [],
+  templateUrl: './announcement.html',
+  styleUrl: './announcement.css'
+})
+export class AnnouncementComponent{
+  
+  
 }
