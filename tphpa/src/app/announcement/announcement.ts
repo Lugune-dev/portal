71e8020ccaf815429768cbe001e4daa1b8736e74
Lugune-dev <luguneshadrack@gmail.com>
2026-01-16 13:00:14 +0300

    some change is done

--- tphpa/src/app/announcement/announcement.ts
+++ tphpa/src/app/announcement/announcement.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-announcement',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './announcement.html',
   styleUrl: './announcement.css'
 })
