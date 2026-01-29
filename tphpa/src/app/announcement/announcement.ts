694108564596f346a5ec074ae87b01e7ef23c3f2
Lugune-dev <luguneshadrack@gmail.com>
2026-01-29 14:47:43 +0300

    some changes is done

--- tphpa/src/app/announcement/announcement.ts
+++ tphpa/src/app/announcement/announcement.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnnouncementService } from '../services/announcement';
 
 @Component({
   selector: 'app-component',
   templateUrl: './announcement.html',
-  styleUrls: ['./announcement.css']
+  styleUrls: ['./announcement.css'],
+  imports: [CommonModule]
  })
-export class AnnouncementComponent {
+export class AnnouncementComponent implements OnInit {
+    updates: any[] = [];
 
+    constructor(private announcementService: AnnouncementService) { }
+    ngOnInit(): void {
+    this.announcementService.getAnnouncement().subscribe((data: any[]) =>{
+     this.updates=data;
+    });
+    }
 }
